6f73409d45ed0a61709b04a0d5b80a3f3da942f2
Triumph Oguine <159278941+triumphoguine@users.noreply.github.com>
2024-12-14 03:32:14 +0100

    Add files via upload

---
+++ sos/script.js
@@ -0,0 +1,19 @@
+
+  
+
+  function subscribeNewsletter(event) {
+    event.preventDefault();
+    alert("Thank you for subscribing!");
+}
+
+// Select the hamburger menu and the navbar
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const navbar = document.querySelector('.navbar');
+
+// Add event listener to the hamburger menu
+hamburgerMenu.addEventListener('click', () => {
+  // Toggle the 'active' class on the navbar
+  navbar.classList.toggle('active');
+});
+
+
